433227f2174380417763de62004fc7b8b1af59a8
david <ostermann.david@gmail.com>
2018-06-11 15:09:11 +0200

    simple liste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  users = [
+    {
+      id: 0,
+      name: 'Aisha Eichmann',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/malykhinv/128.jpg',
+      jobTitle: 'Customer Division Assistant'
+    },
+    {
+      id: 1,
+      name: 'Calista Skiles',
+      image: 'https://s3.amazonaws.com/uifaces/faces/twitter/nessoila/128.jpg',
+      jobTitle: 'Dynamic Solutions Orchestrator'
+    },
+    {
+      id: 2,
+      name: 'Kristofer Hermiston',
+      image:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/ernestsemerda/128.jpg',
+      jobTitle: 'Dynamic Markets Planner'
+    }
+  ];
+  selectedUser;
+  selectUser(user, e) {
+    e.preventDefault();
+    this.selectedUser = user;
+  }
 }
